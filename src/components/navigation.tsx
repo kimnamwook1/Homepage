4c54a0bbfc6eeb0f11217409e92f8da22aa7884c
Why_So_Serious <91526214+kimnamwook1@users.noreply.github.com>
2025-03-26 17:35:19 +0900

    텍스트 로고를 이미지 로고로 변경

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 
 type NavigationProps = {
   activePage?: string | null;
@@ -11,7 +12,13 @@ const Navigation = ({ activePage = null, transparent = false }: NavigationProps)
       {/* Logo */}
       <div className="header">
         <Link href="/" className="logo">
-          The JPC
+          <Image 
+            src="/images/TheJPC_logo_cropped.png" 
+            alt="The JPC Logo" 
+            width={120} 
+            height={48} 
+            priority
+          />
         </Link>
       </div>
 
