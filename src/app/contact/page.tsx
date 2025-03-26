3ed2ce52b12cbea688036caaebc7d4c4ec169174
KoreanJoker <namug014@gmail.com>
2025-03-27 02:24:31 +0900

    10

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -7,12 +7,12 @@ import useDarkMode from "@/lib/useDarkMode";
 export default function Contact() {
   const isDarkMode = useDarkMode();
   
-  // 다크모드에 따라 로고 이미지 선택 - 상단에는 항상 흰색 로고 사용
-  const logoSrc = "/images/TheJPC_logo_White.png";
+  // 다크모드에 따라 로고 이미지 선택
+  const logoSrc = isDarkMode ? "/images/TheJPC_logo_White.png" : "/images/thejpc-logo.svg";
   return (
     <main className="min-h-screen flex flex-col">
       {/* 상단 섹션 - Bordeaux 빨간색 배경 */}
-      <section className="bg-[#9B2335] text-white pt-5 h-1/2 flex flex-col justify-center relative border-b border-black">
+      <section className="bg-[#9B2335] text-white h-[50vh] flex flex-col justify-between relative">
         {/* 로고 */}
         <div className="header">
           <Link href="/" className="logo">
@@ -30,24 +30,23 @@ export default function Contact() {
         <Hamburger />
 
         {/* Contact 제목 */}
-        <div className="container mx-auto px-4 text-center">
+        <div className="container mx-auto px-8 pb-20">
           <h1 className="text-8xl md:text-[12rem] font-bold tracking-tighter">Contact</h1>
         </div>
       </section>
 
       {/* 하단 콘텐츠 섹션 */}
-      <section className={`${isDarkMode ? 'bg-[#121212] text-white' : 'bg-white text-black'} py-10 md:pt-20`}>
-        <div className="container mx-auto px-4">
+      <section className={`${isDarkMode ? 'bg-[#121212] text-white' : 'bg-white text-black'} h-[50vh] py-20`}>
+        <div className="container mx-auto px-8">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-start">
             {/* 좌측 섹션 */}
             <div className="md:pr-8">
-              <h2 className="text-5xl md:text-8xl font-bold">Seoul</h2>
-              <div className="h-0.5 w-10 bg-[#9B2335] mt-4"></div>
+              <h2 className="text-6xl md:text-7xl font-bold">Seoul</h2>
             </div>
 
             {/* 우측 섹션 - 서울 주소 */}
             <div className="md:pl-8">
-              <div className="space-y-2 text-lg md:mt-1 mt-6">
+              <div className="space-y-2 text-lg mt-4 md:mt-1">
                 <p>180, Bangbae-ro, Seocho-gu</p>
                 <p>Seoul, Republic of Korea 06586</p>
                 <p className="mt-4">E: jonathankim@thejpc.kr</p>
