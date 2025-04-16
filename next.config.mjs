17523da6cf62bd9aef095962e68408a53cc89bc8
Why_So_Serious <91526214+kimnamwook1@users.noreply.github.com>
2025-04-16 12:19:24 +0900

    change next.config.js for delete standalone option

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  distDir: 'out',
+  // output: 'standalone',
+  // distDir: 'out',
   images: {
     unoptimized: true,
     domains: [
